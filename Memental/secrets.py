8eb18541216a3a0f622fda09a17971189bf239a1
Riazul Islam Rifat <riazulislamrifat928@gmail.com>
2022-01-04 22:23:11 +0600

    first_commit

---
+++ Memental/secrets.py
@@ -0,0 +1,11 @@
+SECRET_KEY = '2lpof@m(wlzzgx7ozk@dmo-qhw-6%6zi3r$o7k%+_5!%#=4ucv'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mementaldb',
+        'USER': 'postgres',
+        'PASSWORD': '12345678',
+        'HOST': 'localhost',
+    }
+}
